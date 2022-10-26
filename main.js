b5c74177faeb184173e55408899dafdee12ccb3e
davronkhamdamov <davaronxamdamov@gmail.com>
2022-10-26 15:15:48 +0500

    sound

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ result.lang = "uz-UZ"
 let btn = document.getElementById('speak')
 btn.addEventListener('click', () => {
     btn.textContent = 'Gapiraver'
+    btn.classList.add('box')
     result.start()
 })
+setTimeout(() => {
+    btn.classList.remove('box')
+}, 10000)
 result.onresult = (e) => {
+    btn.classList.remove('box')
     btn.textContent = 'Bos va Gapir'
     text.textContent = e.results[0][0].transcript;
+    console.log(e.results[0][0].transcript)
 
 }
