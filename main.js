a576612a341bf885e570839f2609928450e198e8
davronkhamdamov <davaronxamdamov@gmail.com>
2022-10-26 14:44:52 +0500

    salom

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ let text = document.getElementById('text')
 result.lang = "uz-UZ"
 let btn = document.getElementById('speak')
 btn.addEventListener('click', () => {
-    btn.textContent = 'gapiraver'
+    btn.textContent = 'Gapiraver'
     result.start()
 })
 result.onresult = (e) => {
+    btn.textContent = 'Bos va Gapir'
     text.textContent = e.results[0][0].transcript;
-    btn.textContent = 'bos va gapir'
 
 }
